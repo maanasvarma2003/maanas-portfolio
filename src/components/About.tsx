3efa5cada1281e69551fefd49772cfae535c0a75
Maanas Varma S H <sh.maanasvarma@gmail.com>
2025-11-24 20:42:00 +0530

    Update resume link in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -79,7 +79,7 @@ const About = () => {
               </p>
               
               <Button 
-                onClick={() => window.open("https://drive.google.com/file/d/1Y0o7sfsKlKB6Nq5C2uNoIWIpr7JTYB1b/view?usp=sharing")}
+                onClick={() => window.open("https://drive.google.com/file/d/1UfMR3O-AzxYfm_DsamEYT599jHvKkUbt/view?usp=sharing")}
                 className="btn-glow px-6 py-3 rounded-lg"
               >
                 <Download className="w-5 h-5 mr-2" />
